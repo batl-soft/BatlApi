ebdbf253cd42c49fa2f20e3213d2d7a544453ae1
Martin Carpentier <martin@carpentier.dk>
2018-04-19 21:56:09 +0200

    Added initial database models

---
+++ BatlApi/DataAccess/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatlApi.DataAccess.Models
+{
+    public class Company
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LogoUrl { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ BatlApi/DataAccess/Models/Point.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatlApi.DataAccess.Models
+{
+    public class Point
+    {
+    }
+}
